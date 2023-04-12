c33370d46d9ab4e8e0250f5092398c568c60dd6d
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2023-04-12 09:27:59 +0300

    Dev deploy test

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 export const HomePage: React.FC = () => {
 	const hello = process.env.NEXT_PUBLIC_TEST || 'world';
 
-	return <div>Hello {hello}</div>;
+	return <div>Hello {hello}. This is test of dev deployment</div>;
 };
